a4bde084ddf263bb32ca35678fdd521a6819afeb
Yoshimaru4 <lordkirito4@gmail.com>
2024-12-13 17:43:28 +0300

    On branch master  Changes to be committed: 	modified:   Dockerfile 	modified:   builder/requirements.txt 	modified:   src/handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,18 +1,42 @@
 """ Example handler file. """
 
 import runpod
+from TTS.api import TTS
+import torch
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+print("device = ", device)
+
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+
+print("Loaded model tts !!")
 
 
 def handler(job):
     """ Handler function that will be used to process jobs. """
     job_input = job['input']
+    text = job_input.get('text')
+    
+    wav = tts.tts(
+        text=text,
+        speaker="Ana Florence",
+        language="ru",
+    )
 
-    name = job_input.get('name', 'World')
+    print("ran inference successfully !!!!!!")
+    print("type(wav): ", type(wav))
 
-    return f"Hello, {name}!"
+    json_data = {
+        "text": text, 
+        "wav": [float(x) for x in wav],  # FastAPI does not support numpy types, only support native python types
+        "output_sample_rate": tts.config.audio.output_sample_rate,
+        "sample_rate": tts.config.audio.sample_rate,
+    }
+
+    return json_data
 
 
 runpod.serverless.start({"handler": handler})
