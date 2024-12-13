0b5d951d36103fbe71265e3e3725a8175e4ad8ad
Yoshimaru4 <lordkirito4@gmail.com>
2024-12-13 22:32:33 +0300

    modified:   Dockerfile 	modified:   src/handler.py

--- src/handler.py
+++ src/handler.py
@@ -10,7 +10,7 @@ import torch
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 print("device = ", device)
 
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 print("Loaded model tts !!")
 
